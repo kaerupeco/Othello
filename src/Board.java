3f5f6be24e3053e037881491af91205653b9834e
kaerupecomain <c011449032@edu.teu.ac.jp>
2017-08-05 22:01:02 +0900

    初期盤面を定義

--- src/Board.java
+++ src/Board.java
@@ -1,55 +1,30 @@
 import java.security.spec.ECParameterSpec;
+import java.util.Scanner;
 
 public class Board {
-        //８×８の盤面を定義
-        private final int[][] board = new int[8][8];
+    //盤面の広さ、空のマス、自分の石、相手の石を定義
+    private static final int L = 8;
+    private static final int EMPTY = 0;
+    private final static int BLACK = 1;
+    private final static int WHITE = 2;
+    public static int TURN = BLACK;
 
-        //空状態、自分の石、相手の石を定義
-        private final static int EMPTY = 0;
-        private final static int ZIBUN = 1;
-        private final static int AITE = 2;
+    //盤面を定義
+    private final int[][] board = new int[L][L];
 
-        //コンストラクタで盤面を空で初期化
-        public Board(){
-            for(int i = 0; i<8; i++){
-                for(int j = 0; i<8; i++){
-                    board[i][j] = EMPTY;
-                }
+    //コンストラクタで盤面を空で初期化、白と黒の初期配置
+    public Board() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; i < 8; i++) {
+                board[i][j] = EMPTY;
             }
         }
-
-    //指定した位置が空かどうか調べる
-    public boolean isEmpty() {
-        return false;
-    }
-
-    //指定した位置に自分の石があるか調べる
-    public boolean isZibun(){
-        return  false;
-    }
-
-    //指定した位置に相手の石があるか調べる
-    public boolean isAite(){
-        return false;
-    }
-
-    //指定した位置に自分の石を置く
-    public void put(){
-
-    }
-
-    //石を置いたときに裏返せる石の数を数える
-    public int tryPut(){
-        return 0;
-    }
-
-    //自分の石を数える
-    public int countZibun(){
-        return 0;
-    }
-
-    //相手の石を数える
-    public int countAite(){
-        return 0;
+        board[3][3] = WHITE;
+        board[4][4] = WHITE;
+        board[3][4] = BLACK;
+        board[4][3] = BLACK;
     }
 }
+
+
+
---
+++ src/Turn.java
@@ -0,0 +1,5 @@
+public class Turn {
+    private static void turn(){
+
+    }
+}
